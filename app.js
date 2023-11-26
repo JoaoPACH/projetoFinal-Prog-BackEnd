7174cbfcc5ece5e167712eebf64a1f3255f8bf91
João Pedro C. Pacheco <joaopach94@gmail.com>
2023-11-26 17:48:36 -0300

    Inicializacao da aplicacao e instalacao das bibliotecas express e nodemon.

---
+++ app.js
@@ -0,0 +1,26 @@
+/**
+ * Importação do Framework express, um framework web para NodeJS responsável por encapsular as requisições e respostas HTTP.
+ * Para inicializar o express, vamos usar a constante app, assim podemos iniciar os middlewares que serão utilizados no projeto.
+*/
+const express = require('express');
+const app = express();
+
+/**
+  * Códigos responsáveis por rodar os middlewares da aplicação. Os middlewares são funções responsáveis por processarem
+  * uma requisição/resposta HTTP. Eles podem executar um código, alterar objetos de requisição e resposta,
+  * finalizar uma requisição HTTP ou até mesmo chamar outro middleware.
+*/
+ 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+/**
+  * Código responsável por inicializar o servidor na porta indicada acima,
+  * pelas variável 'port', no caso, na porta 3001.
+*/
+ 
+app.listen(3001, () => {
+  console.log('Aplicação rodando ...');
+});
+
+
